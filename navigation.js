0ada09ed43efd1ae26ec4b5fd95f4b1b6ff7ecd4
esmith-06 <esmith06@iastate.edu>
2026-09-08 13:10:33 -0500

    Refactor navigation to use unordered list and highlight active link
    Updated navigation structure to use an unordered list for better semantics and added active link highlighting.

--- navigation.js
+++ navigation.js
@@ -1,13 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const navigation = `
-    <nav class="navbar">
-      <a href="index.html">About Me</a>
-      <a href="internships.html">Internships</a>
-      <a href="entrepreneurship.html">Entrepreneurship</a>
-      <a href="iowa-state.html">Iowa State</a>
-      <a href="future.html">Future</a>
-    </nav>
-  `;
 
-  document.getElementById("navigation").innerHTML = navigation;
+    const navigation = document.getElementById("navigation");
+
+    navigation.innerHTML = `
+        <nav class="main-nav">
+            <ul>
+                <li>
+                    <a href="index.html">About Me</a>
+                </li>
+
+                <li>
+                    <a href="internships.html">Internships</a>
+                </li>
+
+                <li>
+                    <a href="entrepreneurship.html">Entrepreneurship</a>
+                </li>
+
+                <li>
+                    <a href="iowa-state.html">Iowa State</a>
+                </li>
+
+                <li>
+                    <a href="future.html">Future</a>
+                </li>
+            </ul>
+        </nav>
+    `;
+
+
+    /* Highlight the page the user is currently on */
+
+    const currentPage =
+        window.location.pathname.split("/").pop() || "index.html";
+
+    const navLinks = document.querySelectorAll(".main-nav a");
+
+    navLinks.forEach(link => {
+
+        const linkPage = link.getAttribute("href");
+
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+
+    });
+
 });
